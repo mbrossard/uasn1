18e6c1e12fef16e808b70402453c5b2b73c46251
Mathias Brossard <mathias@brossard.org>
2015-12-10 00:05:33 -0800

    Adding some utility functions

---
+++ src/utils.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2015 Mathias Brossard
+ */
+
+#include "utils.h"
+
+
+int uasn1_write_encoded(uasn1_item_t *element, char *filename)
+{
+    uasn1_buffer_t *buffer = uasn1_buffer_new(64);
+
+    uasn1_encode(element, buffer);
+    uasn1_write_buffer(buffer, filename);
+    uasn1_buffer_free(buffer);
+    return 0;
+}
+
+#ifdef HAVE_OPENSSL
+
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+
+int uasn1_write_base64_buffer(uasn1_buffer_t *buffer, FILE *f)
+{
+    BIO *bio, *b64;
+    int rv = -1;
+    unsigned int written;
+    if(f) {
+        bio = BIO_new_fp(f, BIO_NOCLOSE);
+        b64 = BIO_new(BIO_f_base64());
+        bio = BIO_push(b64, bio);
+        written = BIO_write(bio, buffer->buffer, buffer->current);
+        if(written == buffer->current) {
+            rv = 0;
+        }
+        rv = BIO_flush(bio);
+        BIO_free_all(bio);
+    }
+    return rv;
+}
+
+int uasn1_buffer_dumpBase64(uasn1_buffer_t *buffer, char *filename)
+{
+    BIO *bio, *b64;
+    unsigned char inbuf[512];
+    int inlen;
+    int rv = -1;
+    FILE *f = fopen(filename, "r");
+
+    if(f != NULL) {
+        rv = 0;
+        b64 = BIO_new(BIO_f_base64());
+        bio = BIO_new_fp(f, BIO_NOCLOSE);
+        bio = BIO_push(b64, bio);
+        while((inlen = BIO_read(bio, inbuf, sizeof(inbuf))) > 0) {
+            uasn1_buffer_push(buffer, inbuf, inlen);
+        }
+        BIO_free_all(bio);
+        fclose(f);
+    }
+    return rv;
+}
+
+uasn1_item_t *uasn1_bn_to_asn1(BIGNUM *bn)
+{
+    int l,b;
+    uasn1_item_t *integer;
+    b = BN_num_bits(bn);
+    l = ((b == 0)? 0 : (( b / 8 ) + 1));
+    if((integer = uasn1_item_new(uasn1_integer_type))) {
+        integer->value.string.flags = 0;
+        integer->value.string.size = l + 4;
+        integer->value.string.string = (unsigned char *)
+            malloc((l + 4) * sizeof(unsigned char));
+        if(integer->value.string.string == NULL) {
+            free(integer);
+            integer = NULL;
+        } else {
+            integer->value.string.size = BN_bn2bin(bn, integer->value.string.string);
+            integer->value.string.flags = bn->neg;
+        }
+    }
+    return integer;
+}
+
+#endif
---
+++ src/utils.h
@@ -0,0 +1,38 @@
+#ifndef UASN1_UTILS_H
+#define UASN1_UTILS_H
+
+/*
+ * Copyright © 2015 Mathias Brossard
+ */
+
+#include "config.h"
+#include "uasn1.h"
+
+#ifdef HAVE_OPENSSL
+#include <openssl/bn.h>
+#endif
+
+/** @file utils.h */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Encodes an uasn1_item_t object and write to file. */
+int uasn1_write_encoded(uasn1_item_t *item, char *filename);
+
+#ifdef HAVE_OPENSSL
+/** Converts an OpenSSL BIGNUM structure to a ASN1Element integer. */
+uasn1_item_t *uasn1_bn_to_asn1(BIGNUM *bn);
+
+/** Writes a buffer in Base64 format into file. */
+int uasn1_write_base64_buffer(uasn1_buffer_t *buffer, FILE *f);
+
+/** Reads a Base64 encoded file into buffer */
+int uasn1_buffer_dumpBase64(uasn1_buffer_t *buffer, char *filename);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif

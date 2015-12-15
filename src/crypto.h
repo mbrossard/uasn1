a193a9f1f58993863f4e86efd8218d260bca8e6d
Mathias Brossard <mathias@brossard.org>
2015-12-14 22:31:58 -0800

    Add cryptography abstractions

---
+++ src/crypto.h
@@ -0,0 +1,41 @@
+#ifndef UASN1_CRYPTO_H
+#define UASN1_CRYPTO_H
+
+/*
+ * Copyright © 2015 Mathias Brossard
+ */
+
+#include "cryptoki.h"
+#include "uasn1.h"
+
+typedef enum {
+    UASN1_RSA   = 0x0,
+    UASN1_ECDSA = 0x1
+} uasn1_asymetric_t;
+
+typedef enum {
+    UASN1_SHA1   = 0x0,
+    UASN1_SHA256 = 0x1,
+    UASN1_SHA384 = 0x2,
+    UASN1_SHA512 = 0x3
+} uasn1_digest_t;
+
+typedef enum {
+    UASN1_PKCS11,
+    UASN1_OPENSSL
+} uasn1_crypto_provider_t;
+
+/* Opaque type for keys */
+typedef struct _uasn1_key_t uasn1_key_t;
+
+uasn1_key_t *uasn1_load_pkcs11_key(CK_FUNCTION_LIST_PTR funcs, CK_SLOT_ID slot,
+                                   CK_OBJECT_CLASS class, CK_BYTE_PTR label);
+
+uasn1_item_t *uasn1_key_get_asn1_public_key(uasn1_key_t *key);
+
+int uasn1_x509_sign_new(uasn1_item_t *tbs,
+                        uasn1_key_t *key,
+                        uasn1_digest_t digest,
+                        uasn1_buffer_t *buffer);
+
+#endif
---
+++ src/key.h
@@ -0,0 +1,36 @@
+#ifndef UASN1_KEY_H
+#define UASN1_KEY_H
+
+/*
+ * Copyright © 2015 Mathias Brossard
+ */
+
+#include "uasn1.h"
+#include "crypto.h"
+#include "cryptoki.h"
+
+#ifdef HAVE_OPENSSL
+#include <openssl/evp.h>
+#endif
+
+typedef struct {
+    CK_FUNCTION_LIST_PTR functions;
+    CK_SLOT_ID slot;
+	CK_OBJECT_HANDLE object;
+    CK_SESSION_HANDLE session;
+    CK_ULONG size;
+    CK_KEY_TYPE type;
+    CK_OBJECT_CLASS class;
+} pkcs11_key_t;
+
+typedef struct _uasn1_key_t {
+	uasn1_crypto_provider_t provider;
+	union {
+#ifdef HAVE_OPENSSL
+		EVP_PKEY *openssl;
+#endif
+        pkcs11_key_t pkcs11;
+	};
+} uasn1_key_t;
+
+#endif

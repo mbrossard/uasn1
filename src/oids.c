85650c04e1378c52e0ab004c5ad0b0483494e2b4
Mathias Brossard <mathias@brossard.org>
2015-12-17 00:20:07 -0800

    Add OID functions

---
+++ src/oids.c
@@ -0,0 +1,100 @@
+/*
+ * Copyright © 2015 Mathias Brossard
+ */
+
+#include "oids.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+static uasn1_item_t *uasn1_decode_oid(uasn1_item_t *element,
+                                      uasn1_buffer_t *buffer,
+                                      unsigned int length)
+{
+    unsigned int i, j = 1;
+    unsigned int *v;
+    unsigned char c;
+
+    /* Protection against buffer overflow */
+    if((buffer->current - buffer->seek) < length) {
+        length = buffer->current - buffer->seek;
+    }
+
+    /* How many elements ? */
+    for(i = 0; i < length; i++) {
+        if((buffer->buffer[i + buffer->seek] < 0x80)) j++;
+    }
+
+    v = (unsigned int *) malloc((j) * sizeof(unsigned int));
+    i = 1;
+    do {
+        c = uasn1_buffer_get(buffer);
+        if(i == 1) {
+            v[0] = c / 40;
+            v[1] = c % 40;
+        } else {
+            v[i] = (v[i] << 7) | (c & 0x7F);
+        }
+
+        if (c < 0x80) {
+            i++;
+            if(i < j) {
+                v[i] = 0;
+            }
+        }
+    } while(i < j);
+    element->value.oid.elements = v;
+    element->value.oid.size = j;
+    return element;
+}
+
+uasn1_item_t *uasn1_get_oid_by_name(char *name)
+{
+  uasn1_item_t *ret;
+  uasn1_buffer_t *buffer;
+  unsigned int i;
+
+  for(i = 0; oidtable[i].name != NULL; i++)
+	{
+	  if(strcmp(name, oidtable[i].name) == 0)
+		{
+		  ret = uasn1_item_new(uasn1_oid_type);
+		  buffer = uasn1_buffer_new(oidtable[i].size);
+		  uasn1_buffer_push(buffer, oidtable[i].oid, oidtable[i].size);
+		  uasn1_decode_oid(ret, buffer, oidtable[i].size);
+		  uasn1_buffer_free(buffer);
+		  return ret;
+		}
+	}
+  return NULL;
+}
+
+char *uasn1_get_oid(uasn1_item_t *oid)
+{
+    char *name = NULL;
+    uasn1_buffer_t *buffer = uasn1_buffer_new(16);
+    unsigned int i, j;
+
+    uasn1_encode(oid, buffer);
+
+    for(i = 0; oidtable[i].name != NULL; i++) {
+        if(oidtable[i].size == buffer->current - 2) {
+            int diff = 0;
+            for (j = 0; ((j < oidtable[i].size) && (diff == 0)) ; j++) {
+                if((buffer->buffer[j + 2]) != (oidtable[i].oid[j])) {
+                    diff = 1;
+                }
+            }
+            if(diff == 0) {
+                j = strlen(oidtable[i].name) + 1;
+                name = (char *)malloc(j);
+                memcpy(name, oidtable[i].name, j);
+                goto end;
+			}
+		}
+	}
+
+ end:
+    uasn1_buffer_free(buffer);
+    return name;
+}
---
+++ src/oids.h
@@ -0,0 +1,42 @@
+#ifndef OIDS_H
+#define OIDS_H
+
+/*
+ * Copyright © 2015 Mathias Brossard
+ */
+
+#include "uasn1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Look-up name in OID database
+ * @param [in] name Pointer to <tt>NULL</tt>-terminated string
+ * @returns Pointer to uasn1_item_t structure if found
+ * @returns @c NULL otherwise
+ */
+uasn1_item_t *uasn1_get_oid_by_name(char *name);
+
+/**
+ * @brief Look-up OID in database
+ * @param [in] name Pointer to uasn1_item_t structure.
+ * @returns Pointer to <tt>NULL</tt>-terminated string if found
+ * @returns @c NULL otherwise
+ */
+char *uasn1_get_oid(uasn1_item_t *oid);
+
+
+typedef struct
+{
+  char *name;
+  unsigned int size;
+  unsigned char *oid;
+} oidelement;
+
+extern unsigned char oids[];
+extern oidelement oidtable[];
+
+
+#endif

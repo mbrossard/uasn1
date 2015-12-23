4e40b493fffd51172c9c16837628c6ed614bc8d3
Mathias Brossard <mathias@brossard.org>
2015-12-23 00:27:57 -0800

    Add sha256 OID

--- src/oidsdb.c
+++ src/oidsdb.c
@@ -96,82 +96,84 @@ unsigned char oids[] =
   /* ocspServiceLocator [367] = 1, 3, 6, 1, 5, 5, 7, 48, 1, 7 -- OCSP */
   0x2b, 0x06, 0x01, 0x05, 0x05, 0x07, 0x30, 0x02,
   /* caIssuers [376] = 1, 3, 6, 1, 5, 5, 7, 48, 2 -- PKIX subject/authority info access descriptor */
+  0x2b, 0x0e, 0x03, 0x02, 0x1a,
+  /* sha1 [384] = 1, 3, 14, 3, 2, 26 -- OIW */
   0x55, 0x04, 0x03,
-  /* commonName [384] = 2, 5, 4, 3 -- X.520 DN component */
+  /* commonName [389] = 2, 5, 4, 3 -- X.520 DN component */
   0x55, 0x04, 0x04,
-  /* surname [387] = 2, 5, 4, 4 -- X.520 DN component */
+  /* surname [392] = 2, 5, 4, 4 -- X.520 DN component */
   0x55, 0x04, 0x05,
-  /* serialNumber [390] = 2, 5, 4, 5 -- X.520 DN component */
+  /* serialNumber [395] = 2, 5, 4, 5 -- X.520 DN component */
   0x55, 0x04, 0x06,
-  /* countryName [393] = 2, 5, 4, 6 -- X.520 DN component */
+  /* countryName [398] = 2, 5, 4, 6 -- X.520 DN component */
   0x55, 0x04, 0x07,
-  /* localityName [396] = 2, 5, 4, 7 -- X.520 DN component */
+  /* localityName [401] = 2, 5, 4, 7 -- X.520 DN component */
   0x55, 0x04, 0x08,
-  /* stateOrProvinceName [399] = 2, 5, 4, 8 -- X.520 DN component */
+  /* stateOrProvinceName [404] = 2, 5, 4, 8 -- X.520 DN component */
   0x55, 0x04, 0x09,
-  /* streetAddress [402] = 2, 5, 4, 9 -- X.520 DN component */
+  /* streetAddress [407] = 2, 5, 4, 9 -- X.520 DN component */
   0x55, 0x04, 0x0a,
-  /* organizationName [405] = 2, 5, 4, 10 -- X.520 DN component */
+  /* organizationName [410] = 2, 5, 4, 10 -- X.520 DN component */
   0x55, 0x04, 0x0b,
-  /* organizationalUnitName [408] = 2, 5, 4, 11 -- X.520 DN component */
+  /* organizationalUnitName [413] = 2, 5, 4, 11 -- X.520 DN component */
   0x55, 0x04, 0x0c,
-  /* title [411] = 2, 5, 4, 12 -- X.520 DN component */
+  /* title [416] = 2, 5, 4, 12 -- X.520 DN component */
   0x55, 0x04, 0x29,
-  /* name [414] = 2, 5, 4, 41 -- X.520 DN component */
+  /* name [419] = 2, 5, 4, 41 -- X.520 DN component */
   0x55, 0x04, 0x2a,
-  /* givenName [417] = 2, 5, 4, 42 -- X.520 DN component */
+  /* givenName [422] = 2, 5, 4, 42 -- X.520 DN component */
   0x55, 0x04, 0x2b,
-  /* initials [420] = 2, 5, 4, 43 -- X.520 DN component */
+  /* initials [425] = 2, 5, 4, 43 -- X.520 DN component */
   0x55, 0x04, 0x2c,
-  /* generationQualifier [423] = 2, 5, 4, 44 -- X.520 DN component */
+  /* generationQualifier [428] = 2, 5, 4, 44 -- X.520 DN component */
   0x55, 0x04, 0x2d,
-  /* uniqueIdentifier [426] = 2, 5, 4, 45 -- X.520 DN component */
+  /* uniqueIdentifier [431] = 2, 5, 4, 45 -- X.520 DN component */
   0x55, 0x04, 0x2e,
-  /* dnQualifier [429] = 2, 5, 4, 46 -- X.520 DN component */
+  /* dnQualifier [434] = 2, 5, 4, 46 -- X.520 DN component */
   0x55, 0x04, 0x32,
-  /* uniqueMember [432] = 2, 5, 4, 50 -- X.520 DN component */
+  /* uniqueMember [437] = 2, 5, 4, 50 -- X.520 DN component */
   0x55, 0x04, 0x41,
-  /* pseudonym [435] = 2, 5, 4, 65 -- X.520 DN component */
+  /* pseudonym [440] = 2, 5, 4, 65 -- X.520 DN component */
   0x55, 0x04, 0x48,
-  /* role [438] = 2, 5, 4, 72 -- X.520 DN component */
+  /* role [443] = 2, 5, 4, 72 -- X.520 DN component */
   0x55, 0x1d, 0x09,
-  /* subjectDirectoryAttributes [441] = 2, 5, 29, 9 -- X.509 extension */
+  /* subjectDirectoryAttributes [446] = 2, 5, 29, 9 -- X.509 extension */
   0x55, 0x1d, 0x0e,
-  /* subjectKeyIdentifier [444] = 2, 5, 29, 14 -- X.509 extension */
+  /* subjectKeyIdentifier [449] = 2, 5, 29, 14 -- X.509 extension */
   0x55, 0x1d, 0x0f,
-  /* keyUsage [447] = 2, 5, 29, 15 -- X.509 extension */
+  /* keyUsage [452] = 2, 5, 29, 15 -- X.509 extension */
   0x55, 0x1d, 0x10,
-  /* privateKeyUsagePeriod [450] = 2, 5, 29, 16 -- X.509 extension */
+  /* privateKeyUsagePeriod [455] = 2, 5, 29, 16 -- X.509 extension */
   0x55, 0x1d, 0x11,
-  /* subjectAltName [453] = 2, 5, 29, 17 -- X.509 extension */
+  /* subjectAltName [458] = 2, 5, 29, 17 -- X.509 extension */
   0x55, 0x1d, 0x13,
-  /* basicConstraints [456] = 2, 5, 29, 19 -- X.509 extension */
+  /* basicConstraints [461] = 2, 5, 29, 19 -- X.509 extension */
   0x55, 0x1d, 0x14,
-  /* cRLNumber [459] = 2, 5, 29, 20 -- X.509 extension */
+  /* cRLNumber [464] = 2, 5, 29, 20 -- X.509 extension */
   0x55, 0x1d, 0x15,
-  /* cRLReason [462] = 2, 5, 29, 21 -- X.509 extension */
+  /* cRLReason [467] = 2, 5, 29, 21 -- X.509 extension */
   0x55, 0x1d, 0x18,
-  /* invalidityDate [465] = 2, 5, 29, 24 -- X.509 extension */
+  /* invalidityDate [470] = 2, 5, 29, 24 -- X.509 extension */
   0x55, 0x1d, 0x1f,
-  /* cRLDistributionPoints [468] = 2, 5, 29, 31 -- X.509 extension */
+  /* cRLDistributionPoints [473] = 2, 5, 29, 31 -- X.509 extension */
   0x55, 0x1d, 0x20,
-  /* certificatePolicies [471] = 2, 5, 29, 32 -- X.509 extension */
+  /* certificatePolicies [476] = 2, 5, 29, 32 -- X.509 extension */
   0x55, 0x1d, 0x23,
-  /* authorityKeyIdentifier [474] = 2, 5, 29, 35 -- X.509 extension */
+  /* authorityKeyIdentifier [479] = 2, 5, 29, 35 -- X.509 extension */
   0x55, 0x1d, 0x25,
-  /* extKeyUsage [477] = 2, 5, 29, 37 -- X.509 extension */
+  /* extKeyUsage [482] = 2, 5, 29, 37 -- X.509 extension */
   0x55, 0x1d, 0x25, 0x00,
-  /* anyExtendedKeyUsage [480] = 2, 5, 29, 37, 0 -- X.509 extended key usage */
+  /* anyExtendedKeyUsage [485] = 2, 5, 29, 37, 0 -- X.509 extended key usage */
   0x55, 0x1d, 0x26,
-  /* authorityAttributeIdentifier [484] = 2, 5, 29, 38 -- X.509 extension */
+  /* authorityAttributeIdentifier [489] = 2, 5, 29, 38 -- X.509 extension */
   0x55, 0x1d, 0x2e,
-  /* freshestCRL [487] = 2, 5, 29, 46 -- X.509 extension */
+  /* freshestCRL [492] = 2, 5, 29, 46 -- X.509 extension */
   0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01,
-  /* sha-256 [490] = 2, 16, 840, 1, 101, 3, 4, 2, 1 -- NIST Algorithm */
+  /* sha-256 [495] = 2, 16, 840, 1, 101, 3, 4, 2, 1 -- NIST Algorithm */
   0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x02,
-  /* sha-384 [499] = 2, 16, 840, 1, 101, 3, 4, 2, 2 -- NIST Algorithm */
+  /* sha-384 [504] = 2, 16, 840, 1, 101, 3, 4, 2, 2 -- NIST Algorithm */
   0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x03,
-  /* sha-512 [508] = 2, 16, 840, 1, 101, 3, 4, 2, 3 -- NIST Algorithm */
+  /* sha-512 [513] = 2, 16, 840, 1, 101, 3, 4, 2, 3 -- NIST Algorithm */
   0
 };
 
@@ -223,43 +225,44 @@ oidelement oidtable[] =
   { "ocspResponse", 9, oids + 358 },
   { "ocspServiceLocator", 9, oids + 367 },
   { "caIssuers", 8, oids + 376 },
-  { "commonName", 3, oids + 384 },
-  { "surname", 3, oids + 387 },
-  { "serialNumber", 3, oids + 390 },
-  { "countryName", 3, oids + 393 },
-  { "localityName", 3, oids + 396 },
-  { "stateOrProvinceName", 3, oids + 399 },
-  { "streetAddress", 3, oids + 402 },
-  { "organizationName", 3, oids + 405 },
-  { "organizationalUnitName", 3, oids + 408 },
-  { "title", 3, oids + 411 },
-  { "name", 3, oids + 414 },
-  { "givenName", 3, oids + 417 },
-  { "initials", 3, oids + 420 },
-  { "generationQualifier", 3, oids + 423 },
-  { "uniqueIdentifier", 3, oids + 426 },
-  { "dnQualifier", 3, oids + 429 },
-  { "uniqueMember", 3, oids + 432 },
-  { "pseudonym", 3, oids + 435 },
-  { "role", 3, oids + 438 },
-  { "subjectDirectoryAttributes", 3, oids + 441 },
-  { "subjectKeyIdentifier", 3, oids + 444 },
-  { "keyUsage", 3, oids + 447 },
-  { "privateKeyUsagePeriod", 3, oids + 450 },
-  { "subjectAltName", 3, oids + 453 },
-  { "basicConstraints", 3, oids + 456 },
-  { "cRLNumber", 3, oids + 459 },
-  { "cRLReason", 3, oids + 462 },
-  { "invalidityDate", 3, oids + 465 },
-  { "cRLDistributionPoints", 3, oids + 468 },
-  { "certificatePolicies", 3, oids + 471 },
-  { "authorityKeyIdentifier", 3, oids + 474 },
-  { "extKeyUsage", 3, oids + 477 },
-  { "anyExtendedKeyUsage", 4, oids + 480 },
-  { "authorityAttributeIdentifier", 3, oids + 484 },
-  { "freshestCRL", 3, oids + 487 },
-  { "sha-256", 9, oids + 490 },
-  { "sha-384", 9, oids + 499 },
-  { "sha-512", 9, oids + 508 },
+  { "sha1", 5, oids + 384 },
+  { "commonName", 3, oids + 389 },
+  { "surname", 3, oids + 392 },
+  { "serialNumber", 3, oids + 395 },
+  { "countryName", 3, oids + 398 },
+  { "localityName", 3, oids + 401 },
+  { "stateOrProvinceName", 3, oids + 404 },
+  { "streetAddress", 3, oids + 407 },
+  { "organizationName", 3, oids + 410 },
+  { "organizationalUnitName", 3, oids + 413 },
+  { "title", 3, oids + 416 },
+  { "name", 3, oids + 419 },
+  { "givenName", 3, oids + 422 },
+  { "initials", 3, oids + 425 },
+  { "generationQualifier", 3, oids + 428 },
+  { "uniqueIdentifier", 3, oids + 431 },
+  { "dnQualifier", 3, oids + 434 },
+  { "uniqueMember", 3, oids + 437 },
+  { "pseudonym", 3, oids + 440 },
+  { "role", 3, oids + 443 },
+  { "subjectDirectoryAttributes", 3, oids + 446 },
+  { "subjectKeyIdentifier", 3, oids + 449 },
+  { "keyUsage", 3, oids + 452 },
+  { "privateKeyUsagePeriod", 3, oids + 455 },
+  { "subjectAltName", 3, oids + 458 },
+  { "basicConstraints", 3, oids + 461 },
+  { "cRLNumber", 3, oids + 464 },
+  { "cRLReason", 3, oids + 467 },
+  { "invalidityDate", 3, oids + 470 },
+  { "cRLDistributionPoints", 3, oids + 473 },
+  { "certificatePolicies", 3, oids + 476 },
+  { "authorityKeyIdentifier", 3, oids + 479 },
+  { "extKeyUsage", 3, oids + 482 },
+  { "anyExtendedKeyUsage", 4, oids + 485 },
+  { "authorityAttributeIdentifier", 3, oids + 489 },
+  { "freshestCRL", 3, oids + 492 },
+  { "sha-256", 9, oids + 495 },
+  { "sha-384", 9, oids + 504 },
+  { "sha-512", 9, oids + 513 },
   { NULL, 0, NULL }
 };

5a4d882589e2d9beb3d44c1e19ace899c6f2b4c7
Mathias Brossard <mathias@brossard.org>
2015-12-30 00:06:19 -0800

    Add X.509 tests in header

--- tests/tests.h
+++ tests/tests.h
@@ -9,6 +9,13 @@
 #include "crypto.h"
 #include "cryptoki.h"
 
+int request_test(uasn1_key_t *private, uasn1_key_t *public,
+                 uasn1_digest_t digest, char *name);
+int x509_self_test(uasn1_key_t *private, uasn1_key_t *public,
+                   uasn1_digest_t digest, char *name);
+int x509_sign_test(uasn1_key_t *private, uasn1_key_t *public,
+                   uasn1_digest_t digest, char *ca, char *name);
+
 int ocsp_request_test(uasn1_key_t *key, char *name);
 int ocsp_response_test(uasn1_key_t *private, uasn1_digest_t digest, char *name);
 

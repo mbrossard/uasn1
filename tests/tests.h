2c7c988bb5b3a33743760720c5bbc856f1b42c43
Mathias Brossard <mathias@brossard.org>
2015-12-31 00:01:46 -0800

    Move out TSA test

--- tests/tests.h
+++ tests/tests.h
@@ -19,4 +19,9 @@ int x509_sign_test(uasn1_key_t *private, uasn1_key_t *public,
 int ocsp_request_test(uasn1_key_t *key, char *ca, char *name);
 int ocsp_response_test(uasn1_key_t *private, uasn1_digest_t digest, char *name);
 
+int tsa_request_test(CK_FUNCTION_LIST_PTR funcs, CK_SLOT_ID slot,
+                     uasn1_digest_t digest, char *name);
+int tsa_response_test(uasn1_digest_t digest, char *name,
+                      char *crt_path, uasn1_key_t *key);
+
 #endif

fc27a4cc6fe6f1e420a42f8c4405cd0e98a8de75
Mathias Brossard <mathias@brossard.org>
2016-01-01 21:10:15 -0800

    Add ASN.1 re-encoding test

--- tests/test_asn1.c
+++ tests/test_asn1.c
@@ -2,9 +2,10 @@
  * Copyright © 2015 Mathias Brossard
  */
 
-#include <string.h>
-
 #include "uasn1.h"
+
+#include <string.h>
+#include <stdio.h>
 
 char *message = "Hello World...";
 char *time = "010730154741Z";
@@ -14,10 +15,12 @@ unsigned int oid2[] = { 2, 5, 4632, 34523, 346663 };
 
 int main()
 {
-    uasn1_buffer_t *buffer = uasn1_buffer_new(64);
-
+    uasn1_buffer_t *buf1 = uasn1_buffer_new(512);
+    uasn1_buffer_t *buf2 = uasn1_buffer_new(512);
+    uasn1_buffer_t *buf3 = uasn1_buffer_new(512);
     uasn1_item_t *sequence = uasn1_sequence_new(7);
     uasn1_item_t *set = uasn1_set_new(1);
+    uasn1_item_t *decoded;
 
     uasn1_add(set, uasn1_integer_new(0x1F00FF00));
     uasn1_add(sequence, set);
@@ -29,10 +32,26 @@ int main()
     uasn1_add(sequence, uasn1_oid_new(oid, 4));
     uasn1_add(sequence, uasn1_oid_new(oid2, 5));
 
-    uasn1_encode(sequence, buffer);
-    uasn1_write_buffer(buffer, "test.der");
+    uasn1_encode(sequence, buf1);
+    uasn1_write_buffer(buf1, "tests/test_asn1.der");
 
+    uasn1_load_buffer(buf2, "tests/test_asn1.der");
+    decoded = uasn1_decode(buf2);
+    uasn1_encode(decoded, buf3);
+
+    if(buf1->current != buf3->current) {
+        fprintf(stderr, "Encoded value differ in size\n");
+        return -1;
+    }
+    if(memcmp(buf1->buffer, buf3->buffer, buf1->current)) {
+        fprintf(stderr, "Encoded value differ in content\n");
+        return -1;
+    }
+    
+    uasn1_buffer_free(buf1);
+    uasn1_buffer_free(buf2);
+    uasn1_buffer_free(buf3);
     uasn1_free(sequence);
-    uasn1_buffer_free(buffer);
+    uasn1_free(decoded);
     return 0;
 }

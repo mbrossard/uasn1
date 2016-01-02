154bfb5bb5b73851b832deccd2e2ebf7dd2becbf
Mathias Brossard <mathias@brossard.org>
2016-01-02 00:17:19 -0800

    Add CRL test to header

--- tests/tests.h
+++ tests/tests.h
@@ -15,6 +15,7 @@ int x509_self_test(uasn1_key_t *private, uasn1_key_t *public,
                    uasn1_digest_t digest, char *name);
 int x509_sign_test(uasn1_key_t *private, uasn1_key_t *public,
                    uasn1_digest_t digest, char *ca, char *name);
+int crl_test(uasn1_key_t *key, uasn1_digest_t digest, char *name);
 
 int ocsp_request_test(uasn1_key_t *key, char *ca, char *name);
 int ocsp_response_test(uasn1_key_t *private, uasn1_digest_t digest, char *name);

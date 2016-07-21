5209cf972d72af0f29b2b43007c4466b15bcfb09
Mathias Brossard <mathias@brossard.org>
2016-07-20 23:56:56 -0700

    Define sasn1_tag_t type

--- src/sasn1.h
+++ src/sasn1.h
@@ -9,4 +9,12 @@
  * Copyright (C) 2016 Mathias Brossard <mathias@brossard.org>
  */
 
+typedef struct {
+    uasn1_type_t type;
+    uasn1_construct_t construct;
+    uasn1_class_t _class;
+    uasn1_tagging_class_t tag;
+    uint8_t value;
+} sasn1_tag_t;
+
 #endif

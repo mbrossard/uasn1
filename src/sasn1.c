dc0df24f30fcd26cbc71e32542ec06204e50d57d
Mathias Brossard <mathias@brossard.org>
2016-07-21 01:30:32 -0700

    Implement allocator

--- src/sasn1.c
+++ src/sasn1.c
@@ -1,7 +1,18 @@
 #include "sasn1.h"
 
-
 sasn1_t *sasn1_new(size_t size)
 {
-    return NULL;
+    sasn1_t *r = malloc(sizeof(sasn1_t));
+    sasn1_element_t *e = malloc(size * sizeof(sasn1_element_t));
+
+    if((r != NULL) && (e != NULL)) {
+        r->elements = e;
+        r->count = 0;
+        r->size = size;
+    } else {
+        free(r);
+        free(e);
+        r = NULL;
+    }
+    return r;
 }

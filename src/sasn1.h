36957f497986b428c77d44c74944193a1196abad
Mathias Brossard <mathias@brossard.org>
2016-07-26 00:14:54 -0700

    Forward declaration

--- src/sasn1.h
+++ src/sasn1.h
@@ -48,5 +48,6 @@ void sasn1_free(sasn1_t *value);
 size_t sasn1_allocate(sasn1_t *value);
 size_t sasn1_decode_length(uint8_t *ptr, size_t size, size_t *length);
 size_t sasn1_decode(sasn1_t *value, uint8_t *ptr, size_t size, size_t parent, size_t *index);
+size_t sasn1_compute_sizes(sasn1_t *value);
 
 #endif

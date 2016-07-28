3e530a69730ab41dd10b7a2d70ebfc5054eed754
Mathias Brossard <mathias@brossard.org>
2016-07-28 01:31:38 -0700

    Include some basic parsing in test

--- tests/test_sasn1.c
+++ tests/test_sasn1.c
@@ -9,5 +9,17 @@
 
 int main()
 {
+    char *path = "tests/ocsp_rsa1_crt.der";
+    uint8_t input[1024 * 64];
+    size_t l = 0, r;
+    FILE *f = fopen(path, "rb");
+    if(f) {
+        l = fread(input, 1, sizeof(input), f);
+        fclose(f);
+    }
+    fprintf(stderr, "Loaded %zu bytes\n", l);
+    sasn1_t *v = sasn1_new(16);
+    r = sasn1_decode(v, input, l, SIZE_MAX, NULL);
+    fprintf(stderr, "Parsed %zu bytes\n", r);
     return 0;
 }

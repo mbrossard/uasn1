fa96dac110feb3040c4eecc816e4f4140bdeec42
Mathias Brossard <mathias@brossard.org>
2016-08-01 01:40:22 -0700

    Test with more files

--- tests/test_sasn1.c
+++ tests/test_sasn1.c
@@ -9,46 +9,62 @@
 
 int main()
 {
-    char *path = "tests/ocsp_rsa1_crt.der";
+    char *paths[] = {
+        "tests/ec_ca.der",
+        "tests/ec_ca_crl.der",
+        "tests/ocsp_rsa1_crt.der",
+        "tests/ocsp_rsa1_crt_req.der",
+        "tests/ocsp_rsa1_crt_res.der",
+        "tests/ocsp_rsa2_crt.der",
+        "tests/ocsp_rsa2_crt_req.der",
+        "tests/ocsp_rsa2_crt_res.der",
+        "tests/rsa1_ca.der",
+        "tests/rsa1_ca_crl.der",
+        "tests/rsa2_ca.der",
+        "tests/rsa2_ca_crl.der",
+        "tests/tsa_rsa1_crt.der",
+        "tests/tsa_rsa2_crt.der"
+    };
     uint8_t input[1024 * 64];
     uint8_t output[1024 * 64];
-    size_t l = 0, r = 0, err = 0;
-    FILE *f = fopen(path, "rb");
-    if(f) {
-        l = fread(input, 1, sizeof(input), f);
-        fclose(f);
-    } else {
-        fprintf(stderr, "Error opening '%s'\n", path);
-        return 1;
-    }
-    fprintf(stderr, "Loaded %zu bytes\n", l);
-    sasn1_t *v = sasn1_new(16);
-    r = sasn1_decode(v, input, l, SIZE_MAX, NULL);
-    fprintf(stderr, "Parsed %zu bytes\n", r);
-    if(l != r) {
-        fprintf(stderr, "Decoding: sizes do not match got %zu expected %zu\n", r, l);
-        err = 1;
-    }
+    size_t err = 0, i, l, r;
 
-    l = sasn1_compute_sizes(v);
-    if(l != r) {
-        fprintf(stderr, "Computing: sizes do not match got %zu expected %zu\n", r, l);
-        err = 1;
+    for(i = 0; i < (sizeof(paths)/sizeof(char *)); i++) {
+        FILE *f = fopen(paths[i], "rb");
+        if(f) {
+            l = fread(input, 1, sizeof(input), f);
+            fclose(f);
+        }
+        fprintf(stderr, "Loaded %zu bytes\n", l);
+        sasn1_t *v = sasn1_new(16);
+        r = sasn1_decode(v, input, l, SIZE_MAX, NULL);
+        fprintf(stderr, "Parsed %zu bytes\n", r);
+        if(l != r) {
+            fprintf(stderr, "Decoding: sizes do not match got %zu expected %zu\n", r, l);
+            err = 1;
+        }
+    
+        l = sasn1_compute_sizes(v);
+        if(l != r) {
+            fprintf(stderr, "Computing: sizes do not match got %zu expected %zu\n", r, l);
+            err = 1;
+        }
+    
+        fprintf(stderr, "Computed %zu\n", l);
+        l = sasn1_encode(v, output, sizeof(output));
+        fprintf(stderr, "Wrote %zu\n", l);
+        if(l != r) {
+            fprintf(stderr, "Encoding: sizes do not match got %zu expected %zu\n", r, l);
+            err = 1;
+        }
+    
+        if(memcmp(input, output, l) != 0) {
+            fprintf(stderr, "Re-encoding does not match original\n");
+            err = 1;
+            return err;
+        }
+        
+        sasn1_free(v);
     }
-
-    fprintf(stderr, "Computed %zu\n", l);
-    l = sasn1_encode(v, output, sizeof(output));
-    fprintf(stderr, "Wrote %zu\n", l);
-    if(l != r) {
-        fprintf(stderr, "Encoding: sizes do not match got %zu expected %zu\n", r, l);
-        err = 1;
-    }
-
-    if(memcmp(input, output, l) != 0) {
-        fprintf(stderr, "Re-encoding does not match original\n");
-        err = 1;
-    }
-
-    sasn1_free(v);
     return err;
 }
